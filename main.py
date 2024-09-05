524cf5cbff49d8cdc3742fb3c2e7c9e59f64bc5c
Gustavo Medeiros <gustavojmmartins@outlookc.om>
2024-09-05 12:12:02 -0300

    Finalizando CI/CD com docker - videoaula

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 from fastapi import FastAPI
-# from pydantic import BaseModel
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-# class Estudante(BaseModel):
-#     name: str
-#     curso: str
-#     ativo:bool
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo:bool
 
 
 @app.get("/helloworld")
@@ -21,16 +21,16 @@ async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randint(0, 2000)}
 
 
-# @app.post("/estudantes/cadastros")
-# async def create_estudante(estudante: Estudante):
-#     return estudante
+@app.post("/estudantes/cadastros")
+async def create_estudante(estudante: Estudante):
+    return estudante
 
 
-# @app.put("/estudantes/update/{id_studante}")
-# async def update_item(id_estudante: int):
-#     return id_estudante > 0
+@app.put("/estudantes/update/{id_studante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
 
 
-# @app.delete("/estudantes/delete/{id_estudante}")
-# async def delete_estudante(id_estudante: int):
-#     return id_estudante > 0
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
