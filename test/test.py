4f17ff9767814e6a8fecd418f9219e044afcf5af
Gustavo Medeiros <90402262+Gustavojmmedeiros@users.noreply.github.com>
2024-09-26 12:35:28 +0000

    Ajuste de pasta e erros dos testes

---
+++ test/test.py
@@ -0,0 +1,47 @@
+from unittest.mock import patch
+from src.main import *
+
+
+def test_root():
+    result = root()
+    yield result
+    assert result == {"message": "Hello World"}
+
+
+def test_funcaoteste():
+    with patch('random.randint', return_value=1500):
+        result = funcaoteste()
+        yield result
+        
+    assert result == {"teste": True, "num_aleatorio": 1500}
+
+
+def test_create_estudante():
+    estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
+    result = create_estudante(estudante_teste)
+    yield result
+    assert estudante_teste == result
+
+
+def teste_update_estudante_negativo():
+    result = update_estudante(-5)
+    yield result
+    assert not result
+
+
+def teste_update_estudante_positivo():
+    result = update_estudante(10)
+    yield result
+    assert result
+
+
+def test_delete_estudante_negativo():
+    result = delete_estudante(-10)
+    yield result
+    assert not result
+
+
+def test_delete_estudante_positivo():
+    result = delete_estudante(5)
+    yield result
+    assert result
